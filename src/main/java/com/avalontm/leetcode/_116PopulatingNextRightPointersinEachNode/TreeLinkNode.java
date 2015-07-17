966289981ba1f52fcca4d70f1e88e5bfc3e8d327
jiang_s <jiang_s@ctrip.com>
2015-07-17 14:25:50 +0800

    116. Populating Next Right Pointers in Each Node

---
+++ src/main/java/com/avalontm/leetcode/_116PopulatingNextRightPointersinEachNode/TreeLinkNode.java
@@ -0,0 +1,13 @@
+package com.avalontm.leetcode._116PopulatingNextRightPointersinEachNode;
+
+/**
+ * Created by jiang_s on 2015/7/17.
+ */
+public class TreeLinkNode {
+    int val;
+    TreeLinkNode left, right, next;
+
+    TreeLinkNode(int x) {
+        val = x;
+    }
+}
